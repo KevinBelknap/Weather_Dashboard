6b31303492b38cf077104681d4ab4d92f9fed6d9
Kevin Belknap <kevin.belknap@pepsico.com>
2023-05-10 20:46:05 -0500

    Had to restart with yotube tutorial

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,6 @@
-var API_KEY = '90bd873893e586663a4af08821c6ac66'
-var searchBar = document.getElementById('search-bar')
-var searchButton = document.getElementById('search-button')
-console.log(searchBar, searchButton)
-function onClick() {
-    var city_name = searchBar.value
-    console.log(searchBar.value)
-}
+const apiKey = '90bd873893e586663a4af08821c6ac66';
 
-searchButton.onclick = console.log(searchBar.value)
-//var API_URL = `https://api.openweathermap.org/data/2.5/weather?q=` + city_name + `&appid=${API_KEY}`
+const form = document.querySelector('form');
+const cityInput = document.querySelector('#city-input');
+
 
